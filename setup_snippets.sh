1e4455722e57d3bd57f081d0054e75d4d3d1a64f
timing <2891183440@qq.com>
2020-08-06 15:54:21 +0800

    tijiao

--- setup_snippets.sh
+++ setup_snippets.sh
@@ -1,8 +1,4 @@
-#! /bin/bash
-mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
-
-# rm ~/Library/Developer/Xcode/UserData/CodeSnippets
-
+git pull origin master
 SRC_HOME=`pwd`
 ln -s ${SRC_HOME}/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
 echo "done"
